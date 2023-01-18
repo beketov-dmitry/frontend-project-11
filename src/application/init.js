be0af056f9a3f643e92e8f496c2453e5f46080b0
beketov-dmitry <raff2004bd@gmail.com>
2023-01-18 23:43:23 +0300

    Validation done with all possible errors

--- src/application/init.js
+++ src/application/init.js
@@ -1,47 +1,123 @@
-import submit from "./submit"
 import {isEmpty} from "lodash";
 import * as yup from "yup";
 import validate from "./validate";
 import axios from "axios";
+import onChange from "on-change";
+import {value} from "lodash/seq";
+import {setLocale} from "yup";
+
 
 export default () => {
-	const state = {
-       form: {
-		   valid: "true",
+	const initState = {
+		urls: [],
+        form: {
 	       processState: 'filling',
-	       processError: null,
-	       errors: {},
-	       field: {
-			   link: null
-	       },
-		   urls: []
-       }
+	       errorType: null
+        }
 	}
 
-	const schema = yup.string().url("Ссылка должна быть валидным URL").notOneOf(state.form.urls, "RSS уже существует");
+	const elements = {
+		header: {
+			form: document.querySelector(".rss-form"),
+			feedback:  document.querySelector(".feedback "),
+			submit: document.querySelector(".btn"),
+			urlInput: document.querySelector("#url-input")
+		}
+	}
 
-	const form = document.querySelector(".rss-form");
-	const feedback = document.querySelector(".feedback ");
+
+	const errorMessages = {
+		network: {
+			error: 'Ресурс не содержит валидный RSS',
+		},
+		codeOfNetworkErr: "ERR_CODE"
+	};
+
+    const render = (elements, initialState) => (path, value) => {
+        switch (path){
+	        case "form.processState":
+				handlerProcessState(value, elements);
+				break;
+	        case "form.errorType":
+				handlerError(value, elements);
+		        break;
+        }
+	}
+
+	const handlerValidationError = (err) => {
+		if (err.errors[0] === 'not_valid'){
+			return 'Ссылка должна быть валидным URL';
+		}
+		return 'RSS уже существует'
+	}
+
+	const handlerError = (error, elements) => {
+		switch (error.name){
+			case 'ValidationError':
+				elements.header.feedback.innerHTML = handlerValidationError(error);
+				break;
+			case 'AxiosError':
+				elements.header.feedback.innerHTML = "Ошибка сети";
+				break;
+			case 'TypeError':
+				elements.header.feedback.innerHTML = "Ресурс не содержит валидный RSS";
+				break;
+			default:
+				elements.header.feedback.innerHTML = errorMessages.network.error;
+		}
+	}
+
+
+	 const handlerProcessState = (value, elements) => {
+		switch (value){
+			case 'sending':
+				elements.header.submit.disabled = true;
+			    break;
+			case 'success':
+				elements.header.feedback.innerHTML = "RSS успешно загружен";
+				elements.header.feedback.classList.add("text-success");
+				elements.header.feedback.classList.remove("text-danger");
+				elements.header.submit.disabled = false;
+                elements.header.urlInput.innerHTML = "";
+				form.reset();
+				form.focus();
+				break;
+			case 'error':
+				elements.header.feedback.classList.remove("text-success");
+				elements.header.feedback.classList.add("text-danger");
+				elements.header.submit.disabled = false;
+		}
+	}
+
+	const state = onChange(initState, render(elements, initState));
+	const {form} = elements.header;
+	const posts = document.querySelector(".posts")
+	const BASE_URL = `https://allorigins.hexlet.app/get?url=`;
+
 	form.addEventListener("submit", (e) => {
 		e.preventDefault();
 
 		const formData = new FormData(e.target);
-		const url = new URL(formData.get("url"));
-        const {errors, urls} = state.form;
+		const url = formData.get("url");
+		const feeds = state.urls;
 
-		schema.validate(url)
-			.then(() => fetch(url))
-			.catch((e) => {
-				errors["form"] = e.errors;
-			})
-			.then((data) => {
-				urls.push(url);
-			});
+		const schema = yup.string("not_valid").required("not_valid").url("not_valid").notOneOf(feeds, "exist");
 
+
+		schema.validate(url).then(() => {
+			const modifyURL = BASE_URL + encodeURIComponent(url.toString());
+			state.form.processState = "sending";
+			return axios.get(modifyURL);
+		}).then((response) => {
+			console.log(response.data.contents.split("v"));
+			state.form.processState = 'success'
+			state.urls.push(url);
+		}).catch((err) => {
+			state.form.processState = "error";
+			state.form.errorType = err;
+			console.log(err.name);
+		})
 	})
 
-	if(!isEmpty(state.form.errors)){
-		feedback.classList.add("text-danger");
-		feedback.innerHTML = state.form.errors.form;
-	}
+
 }
